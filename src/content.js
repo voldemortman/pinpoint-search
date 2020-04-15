b7d59454340ec572372b4ef8a0bae21d1a822b77
Daniel Mortman <danielmortman@gmail.com>
2020-04-15 20:02:42 +0300

    Create class to find hovered element in website

--- src/content.js
+++ src/content.js
@@ -4,3 +4,35 @@ chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
     sendResponse({ farewell: 'goodbye' });
   }
 });
+
+class ElementSelector {
+  constructor() {
+    this.selectedElementList = null;
+    this.elementsOnClick = new Map();
+  }
+
+  startSearch = () => {
+    while (!this.selectedElementList) {
+      const hoveredElementList = document.querySelectorAll(':hover');
+      hoveredElementList.forEach((hoveredElement, _, elementList) => {
+        const element = hoveredElement;
+        if (!this.elementsOnClick.get(element)) {
+          this.elementsOnClick.set(element, element.onclick);
+          element.onclick = () => {
+            this.selectedElementList = elementList;
+          };
+        }
+      });
+    }
+    this.finishSearch();
+  }
+
+  finishSearch = () => {
+    const elementsOnClickKeys = this.elementsOnClick.keys();
+    let result = elementsOnClickKeys.next();
+    while (result.done) {
+      result.value.onclick = this.elementsOnClick.get(result.value);
+      result = elementsOnClickKeys.next();
+    }
+  }
+}
